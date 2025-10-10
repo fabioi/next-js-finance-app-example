e84aba79ece705fc010a0d88cf830458f89e2242
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-10-10 14:56:30 +0200

    Add icons and color coding to TransactionItem component
    - Enhanced the `TransactionItem` component by introducing a mapping of transaction types to corresponding icons and color classes.
    - Integrated icons from `lucide-react` for visual representation of Income, Expense, Saving, and Investment types.
    - Updated the rendering logic to display the appropriate icon and color based on the transaction type.

--- app/components/transaction-item.js
+++ app/components/transaction-item.js
@@ -1,12 +1,38 @@
 import { useFormatCurrency } from "@/hooks/use-format-currency";
+import { HandCoins, Landmark, PiggyBank, Wallet } from "lucide-react";
 
 export default function TransactionItem({ type, category, description, amount }) {
+
+      const typesMap = {
+            Income: {
+                  icon: HandCoins,
+                  colors: "text-green-500 dark:text-green-500",
+            },
+            Expense: {
+                  icon: Wallet,
+                  colors: "text-red-500 dark:text-red-500",
+            },
+            Saving: {
+                  icon: PiggyBank,
+                  colors: "text-indigo-500 dark:text-yellow-500",
+            },
+            Investment: {
+                  icon: Landmark,
+                  colors: "text-yellow-500 dark:text-yellow-500",
+            },
+      }
+
+
+      // Icon component is an alias for the icon component from the typesMap
+      const IconComponent = typesMap[type].icon;
+      const IconColor = typesMap[type].colors;
       const formatAmount = useFormatCurrency(amount);
 
 
       return (
             <div className="flex w-full items-center">
                   <div className="flex items-center mr-4 grow">
+                        <IconComponent className={`${IconColor} mr-2 w-4 h-4`} />
                         <div>{description}</div>
                   </div>
 
